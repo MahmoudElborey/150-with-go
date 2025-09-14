8ec8c4b0b161e32bbaa0ffe1ada0b2ee236f4c4b
Mahmoud Elborey <mahmoudbubg1234@gmail.com>
2025-09-14 03:59:30 +0300

    first problem with go

---
+++ merge_sorted_array.go
@@ -0,0 +1,36 @@
+import (
+    "fmt"
+)
+
+func shiftElementsRight(nums1 [] int , n int , finalArrayLength int){
+    for i:= finalArrayLength - 1 ; i >= n ; i-- {
+        nums1[i] = nums1[i-n]
+    }
+}
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    finalArrayLength := n + m
+    
+    mergedArrayIndex , secondArrayIndex :=n, 0
+
+    shiftElementsRight(nums1 , n , finalArrayLength)
+    
+
+    for i:=0; i < finalArrayLength ; i++ {
+        if mergedArrayIndex >= 0 && mergedArrayIndex < finalArrayLength && secondArrayIndex < n {
+            if  nums1[mergedArrayIndex] <= nums2[secondArrayIndex] {
+               nums1[i] = nums1[mergedArrayIndex]
+               mergedArrayIndex++
+            } else {
+                nums1[i] = nums2[secondArrayIndex]
+                secondArrayIndex++;
+            }
+        } else if mergedArrayIndex < 0 || mergedArrayIndex >= finalArrayLength {
+            nums1[i] = nums2[secondArrayIndex]
+            secondArrayIndex++;
+        }else {
+            nums1[i] = nums1[mergedArrayIndex]
+            mergedArrayIndex++
+        }
+       
+    }
+}
