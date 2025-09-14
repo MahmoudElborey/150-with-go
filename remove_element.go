5f155f92eb76d763dc50fad1cd8ed743d80bb67e
Mahmoud Elborey <mahmoudbubg1234@gmail.com>
2025-09-14 06:04:39 +0300

    2nd problem with go journey

---
+++ remove_element.go
@@ -0,0 +1,15 @@
+
+func removeElement(nums []int, val int) int {
+    arrayLength := len(nums)
+    var pos int = 0
+    for i:= 0; i < arrayLength; i++ {
+        if nums[i] != val {
+            nums[pos] = nums[i]
+            pos++
+        }
+        
+    }
+    
+    return pos
+
+}
